9180abed756362a5929eb46899d85cd2ca2c524a
lmachens <leon@machens.koeln>
2023-11-01 17:22:14 +0100

    Read map from className

--- lib/nodes.ts
+++ lib/nodes.ts
@@ -40,61 +40,18 @@ export type Node = {
 housing / bahari bay muss angepasst werden, siehe mapName
 {"type":"BP_HousingPlotUnlockDebrisActor_C","x":265200,"y":-87600,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":265200,"y":-86300,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":265200,"y":-85000,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":265200,"y":-83700,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":265200,"y":-82400,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":189800,"y":-87600,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":189800,"y":-86300,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":189800,"y":-85000,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":189800,"y":-83700,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":189800,"y":-82400,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":191100,"y":-87600,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":191100,"y":-86300,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":191100,"y":-85000,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":191100,"y":-83700,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":191100,"y":-82400,"z":5000,"mapName":"bahari-bay","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":192400,"y":-87600,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":192400,"y":-86300,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":192400,"y":-85000,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":192400,"y":-83700,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":192400,"y":-82400,"z":5000,"mapName":"housing","timestamp":1697573646290},{"type":"BP_HousingPlotUnlockDebrisActor_C","x":193700,"y":-87600,"z":5000,"mapName":"housing","timestamp":1697573646290},
  */
-const mapBounds = {
-  fairgrounds: {
-    topLeft: {
-      x: 70000,
-      y: 15000,
-    },
-    bottomRight: {
-      x: 92000,
-      y: 46000,
-    },
-  },
-  "kilima-valley": {
-    topLeft: {
-      x: -54000,
-      y: -44000,
-    },
-    bottomRight: {
-      x: 56000,
-      y: 46000,
-    },
-  },
-  "bahari-bay": {
-    topLeft: {
-      x: 31138.41368584759,
-      y: -130401.24416796268,
-    },
-    bottomRight: {
-      x: 191947.1228615863,
-      y: 30407.46500777605,
-    },
-  },
-  housing: {
-    topLeft: {
-      x: -400000,
-      y: -4000000,
-    },
-    bottomRight: {
-      x: 700000,
-      y: -10000,
-    },
-  },
-} as const;
 
-export function getMapFromCoords(coords: { x: number; y: number }) {
-  for (const mapName in mapBounds) {
-    const bounds = mapBounds[mapName as keyof typeof mapBounds];
-    if (
-      coords.x >= bounds.topLeft.x &&
-      coords.x <= bounds.bottomRight.x &&
-      coords.y >= bounds.topLeft.y &&
-      coords.y <= bounds.bottomRight.y
-    ) {
-      return mapName;
-    }
+export function getMapFromCoords(actor: Actor) {
+  if (actor.className.includes("Village")) {
+    return "kilima-valley";
   }
+  if (actor.className.includes("AZ1")) {
+    return "bahari-bay";
+  }
+  if (actor.className.includes("HousingMaps")) {
+    return "housing";
+  }
+
   return "unknown";
 }
 
@@ -105,7 +62,7 @@ export function toNode(actor: Actor): Node {
     x: actor.x,
     y: actor.y,
     z: actor.z,
-    mapName: getMapFromCoords({ x: actor.x, y: actor.y }),
+    mapName: getMapFromCoords(actor),
     timestamp: Date.now(),
   };
 }
